9f0ab12ad622ea841dd30726e5d2696282d34c68
HuiwonKo <dreams96@naver.com>
2016-07-07 17:13:14 +0900

    initial commit

---
+++ programming/test_regex.py
@@ -0,0 +1,12 @@
+import re
+# regular expression (정규표현식)
+
+def validate_phone_number(number):
+    if re.match(r'C^01[016789][1-9]/d{6,7}$',number):
+        return True
+    return False
+
+print(validate_phone_number('01012341234'))
+print(validate_phone_number('010123412'))
+print(validate_phone_number('01012341234a'))
+
